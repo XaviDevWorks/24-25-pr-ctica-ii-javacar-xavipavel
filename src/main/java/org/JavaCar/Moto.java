ffbb0d3c4f535940f75c2effef6c3599533b6544
Xavier Farrus <xfarmart@gmail.com>
2025-03-14 13:09:19 +0100

    Update Moto.java

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -3,7 +3,7 @@ package org.JavaCar;
 public class Moto extends Vehicle{
     int cilindrada;
     public Moto(String Matr, String Marca, String Model, double preu, Motor Motor,int Rodes, String etiqueta, int places, int cilin){
-        super(Matr,Marca,Model,preu,Motor,Rodes,etiqueta,places);
+        super(Matr,Marca,Model,preu,Motor,Rodes,etiqueta);
         this.cilindrada = cilin;
     }
 }
