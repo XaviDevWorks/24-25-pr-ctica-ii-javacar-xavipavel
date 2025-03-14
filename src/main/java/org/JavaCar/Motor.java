e466047eea824efe840c29cb3009298fcb14fbd6
pavell <pavellupan229@gmail.com>
2025-03-14 12:28:04 +0100

    Comit inicial
    Classes Vehicle i Motor CREADES
    
    Falta--
    definir metodes propis de cada classe

---
+++ src/main/java/org/JavaCar/Motor.java
@@ -0,0 +1,10 @@
+package org.JavaCar;
+
+public class Motor {
+    String alimentacio;
+    double cilindrada;
+    public Motor(String alimentacio, double cil){
+        this.alimentacio = alimentacio;
+        this.cilindrada = cil;
+    }
+}
