50ac35923380110cfc4d5db9deef6f02bd213bb0
Xavier Farrus <xfarmart@gmail.com>
2025-03-14 12:49:50 +0100

    Test

---
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -0,0 +1,22 @@
+package org.JavaCar;
+
+public abstract class Vehicle {
+    String Matricula;
+    String Marca;
+    String Model;
+    double PreuBase;
+    Motor Motor;
+    int Rodes;
+    String EtiquetaAmbiental;
+
+    public Vehicle(String Matr, String Marca, String Model, double preu, Motor Motor,int Rodes, String etiqueta){
+        this.Matricula = Matr;
+        this.Marca = Marca;
+        this.Model = Model;
+        this.PreuBase = preu;
+        this.Motor = Motor;
+        this.Rodes = Rodes;
+        this.EtiquetaAmbiental = etiqueta;
+
+    }
+}
