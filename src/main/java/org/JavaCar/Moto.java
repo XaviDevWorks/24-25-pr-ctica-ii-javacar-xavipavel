1a862aeb517e3d7ed91fc176c7dbff1bb16f367c
Xavier Farrus <xfarmart@gmail.com>
2025-03-14 13:06:55 +0100

    Clase moto

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -2,7 +2,8 @@ package org.JavaCar;
 
 public class Moto extends Vehicle{
     int cilindrada;
-    public Moto(String Matr, String Marca, String Model, double preu, Motor Motor,int Rodes, String etiqueta, int places){
-        super(Matr,Marca,Model,preu,Motor,Rodes,etiqueta,places))
+    public Moto(String Matr, String Marca, String Model, double preu, Motor Motor,int Rodes, String etiqueta, int places, int cilin){
+        super(Matr,Marca,Model,preu,Motor,Rodes,etiqueta,places);
+        this.cilindrada = cilin;
     }
 }
