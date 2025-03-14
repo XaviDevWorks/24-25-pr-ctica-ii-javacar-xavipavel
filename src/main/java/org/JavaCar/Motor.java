687c6a54bc6bc9cca023751543dbe08773367818
Xavier Farrus <xfarmart@gmail.com>
2025-03-14 13:00:25 +0100

    Update Motor.java

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -2,8 +2,8 @@ package org.JavaCar;
 
 public class Motor {
     String alimentacio;
-    double cilindrada;
-    public Motor(String alimentacio, double cil){
+    int cilindrada;
+    public Motor(String alimentacio, int cil){
         this.alimentacio = alimentacio;
         this.cilindrada = cil;
     }
