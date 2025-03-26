b5c4f1f0cc8e18dc9a294ce3cc7f9616837e4997
Xavier Farrus <xfarmart@gmail.com>
2025-03-26 10:27:43 +0100

    Update Cotxe.java

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -2,10 +2,11 @@ package org.JavaCar;
 
 public class Cotxe extends Vehicle implements Llogable{
     private int nombrePlaces;
-    
+
     public Cotxe(String Matr, String Marca, String Model, double preu, int places, Motor Motor,Roda[] Rodes){
         super(Matr,Marca,Model,preu,Motor,Rodes);
         this.nombrePlaces = places;
+        this.EtiquetaAmbiental = EtiquetaAmbiental.obtenerEtiqueta(this);
     }
     public int getNombrePlaces(){
         return nombrePlaces;
