23233f68f30a61beb589e6871f7260c2714ea6ad
Xavier Farrus <xfarmart@gmail.com>
2025-03-26 09:53:35 +0100

    Update EtiquetaAmbiental.java

--- src/main/java/EtiquetaAmbiental.java
+++ src/main/java/EtiquetaAmbiental.java
@@ -6,4 +6,28 @@ public class EtiquetaAmbiental {
         ECO,
         CERO
     }
+
+    public static TipoEtiqueta obtenerEtiqueta(Vehiculo vehiculo) {
+        Motor motor = vehiculo.getMotor();
+        
+        if (vehiculo.esElectrico() || motor.esHibridoEnchufable40km()) {
+            return TipoEtiqueta.CERO;
+        } 
+        else if (motor.esHibrido() || motor.usaGas()) {
+            return TipoEtiqueta.ECO;
+        } 
+        else if (vehiculo.esGasolina() && vehiculo.añoMatriculacion >= 2006 && motor.cumpleNormativaEuro(4)) {
+            return TipoEtiqueta.C;
+        } 
+        else if (vehiculo.esDiesel() && vehiculo.añoMatriculacion >= 2014 && motor.cumpleNormativaEuro(6)) {
+            return TipoEtiqueta.C;
+        } 
+        else if ((vehiculo.esGasolina() && vehiculo.añoMatriculacion >= 2000 && motor.cumpleNormativaEuro(3)) ||
+                   (vehiculo.esDiesel() && vehiculo.añoMatriculacion >= 2006 && motor.cumpleNormativaEuro(4))) {
+            return TipoEtiqueta.B;
+        } 
+        else {
+            return TipoEtiqueta.SIN_ETIQUETA;
+        }
+    }
 }
