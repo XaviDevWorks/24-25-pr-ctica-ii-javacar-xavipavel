7ca30ff4eddb8246933c3c2cbf19fb03c9fe8235
Xavier Farrus <xfarmart@gmail.com>
2025-03-26 09:56:41 +0100

    Update Motor.java

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -1,10 +1,32 @@
 package org.JavaCar;
 
 public class Motor {
-    String alimentacio;
-    int cilindrada;
-    public Motor(String alimentacio, int cil){
-        this.alimentacio = alimentacio;
-        this.cilindrada = cil;
+    private String tipus;
+    private int potencia;
+    private int normativaEuro;
+
+    public Motor(String tipus, int potencia) {
+        this.tipus = tipus;
+        this.potencia = potencia;
+        this.normativaEuro = calcularNormativaEuro(potencia);
+    }
+
+    public String getTipus() {
+        return tipus;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public boolean cumpleNormativaEuro(int normativa) {
+        return this.normativaEuro >= normativa;
+    }
+
+    private int calcularNormativaEuro(int potencia) {
+        if (potencia >= 2014) return 6;
+        if (potencia >= 2006) return 4;
+        if (potencia >= 2000) return 3;
+        return 0;
     }
 }
