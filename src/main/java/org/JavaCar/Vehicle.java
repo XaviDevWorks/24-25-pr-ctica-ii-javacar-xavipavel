0ad15c8b41d7bfdbae608b3f7c40e27f5eefad53
Xavier Farrus <xfarmart@gmail.com>
2025-03-26 09:55:45 +0100

    Update Vehicle.java

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,22 +1,62 @@
 package org.JavaCar;
 
-public abstract class Vehicle {
-    String Matricula;
-    String Marca;
-    String Model;
-    double PreuBase;
-    Motor Motor;
-    int Rodes;
-    String EtiquetaAmbiental;
-
-    public Vehicle(String Matr, String Marca, String Model, double preu, Motor Motor,int Rodes, String etiqueta){
-        this.Matricula = Matr;
-        this.Marca = Marca;
-        this.Model = Model;
+public abstract class Vehicle implements Llogable {
+    protected String matricula;
+    protected String marca;
+    protected String model;
+    protected double PreuBase;
+    protected Motor Motor;
+    protected Roda[] Rodes;
+    protected boolean disponibilidad = true;
+    
+    public Vehicle(String Matr, String Marca, String Model, double preu, Motor Motor, Roda[] Rodes) {
+        this.matricula = Matr;
+        this.marca = Marca;
+        this.model = Model;
         this.PreuBase = preu;
         this.Motor = Motor;
         this.Rodes = Rodes;
-        this.EtiquetaAmbiental = etiqueta;
+    }
 
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPreuBase() {
+        return (int) PreuBase;
+    }
+
+    public Motor getMotor() {
+        return Motor;
+    }
+
+    public Roda[] getRodes() {
+        return Rodes;
+    }
+
+    public double calcularPreu(int dies) {
+        return this.PreuBase * dies;
+    }
+
+    public String printVehicle() {
+        String form = "Matricula: " + getMatricula() + "\n" +
+                      "Marca: " + getMarca() + "\n" +
+                      "Model: " + getModel() + "\n" +
+                      "PreuBase: " + getPreuBase() + "\n" +
+                      "Tipus(Motor): " + Motor.getTipus() + "\n" +
+                      "Potencia(Motor): " + Motor.getPotencia() + "\n" +
+        for (Roda i : Rodes) {
+            form += "Roda(marca): " + i.getMarca() + "\nRoda(Diametre): " + i.getDiametre() + "\n";
+        }
+        System.out.println(form);
+        return form;
     }
 }
